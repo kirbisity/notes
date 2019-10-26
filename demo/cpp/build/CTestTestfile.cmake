5aac7f0896630aea127b9bfbb23d9c58f3a24102
kirbisity <zhuyanlin96@gmail.com>
2019-10-26 15:46:02 -0400

    readded files for gitignore to take effect

--- demo/cpp/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yanlinzhu/dropbox/csplayground/notes/demo/cpp
-# Build directory: /Users/yanlinzhu/dropbox/csplayground/notes/demo/cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- demo/cpp/build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yanlinzhu/dropbox/csplayground/notes/demo/cpp/test
-# Build directory: /Users/yanlinzhu/dropbox/csplayground/notes/demo/cpp/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
